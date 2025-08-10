444bd313dd4ebbf857e68505a51bc203b3fa8950
Talupula Naveen <stylistar05@gmail.com>
2025-08-10 17:12:54 +0530

    Add multiple accordian functionality

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -3,24 +3,49 @@ import React, { useState } from "react";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
+  const [activate, setActivate] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   const handleSingle = (comingId) => {
     console.log(comingId);
     setSelected(comingId === selected ? null : comingId);
   };
 
+  const handleMultiple = (comingId) => {
+    console.log(comingId);
+    let tempMultiple = [...multiple];
+    const findIndex = tempMultiple.indexOf(comingId);
+
+    if (findIndex === -1) tempMultiple.push(comingId);
+    else tempMultiple.splice(findIndex, 1);
+    setMultiple(tempMultiple);
+  };
+
   return (
     <div>
       <div>
+        <button onClick={() => setActivate(!activate)}>
+          {activate ? "Disable Multi Selection" : "Enable Multi Selection"}
+        </button>
         {data && data.length > 0 ? (
           data.map((eachItem) => {
             const { id, question, answer } = eachItem;
             return (
-              <div>
-                <div className="card" onClick={() => handleSingle(id)}>
+              <div key={id}>
+                <div
+                  className="card"
+                  onClick={
+                    activate ? () => handleMultiple(id) : () => handleSingle(id)
+                  }
+                >
                   <h3>{question}</h3>
-                  <span>+</span>
-                  {selected===id?<p>{answer}</p>:<p>{null}</p>}
+                  <span>
+                    {selected === id || multiple.includes(id) ? "-" : "+"}
+                  </span>
+                  {selected === id ||
+                  multiple.includes(id) /* multiple.indexOf(id) !== -1 */ ? (
+                    <p>{answer}</p>
+                  ) : null}
                 </div>
               </div>
             );
