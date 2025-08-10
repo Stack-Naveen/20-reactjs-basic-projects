d039f5c880953fc5d355f324f2060b28da6f85e2
Talupula Naveen <stylistar05@gmail.com>
2025-08-10 13:35:49 +0530

    add single accordian functionality

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,3 +1,34 @@
+import data from "./data";
+import React, { useState } from "react";
+
 export default function Accordian() {
-  return <div>added accordian component</div>;
+  const [selected, setSelected] = useState(null);
+
+  const handleSingle = (comingId) => {
+    console.log(comingId);
+    setSelected(comingId === selected ? null : comingId);
+  };
+
+  return (
+    <div>
+      <div>
+        {data && data.length > 0 ? (
+          data.map((eachItem) => {
+            const { id, question, answer } = eachItem;
+            return (
+              <div>
+                <div className="card" onClick={() => handleSingle(id)}>
+                  <h3>{question}</h3>
+                  <span>+</span>
+                  {selected===id?<p>{answer}</p>:<p>{null}</p>}
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <p>No Data</p>
+        )}
+      </div>
+    </div>
+  );
 }
