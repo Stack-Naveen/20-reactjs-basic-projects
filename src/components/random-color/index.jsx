5b0e5faafa88c6159f2560a24b839ffdf529e9df
Talupula Naveen <stylistar05@gmail.com>
2025-08-12 16:04:53 +0530

    Add handleHex and handleRgb functionalities

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,7 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function RandomColor() {
   const [color, setColor] = useState("black");
+  const [typeOfColor, setTypeOfColor] = useState("HEX");
+
+  function randomColor(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function handleHex() {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[Math.floor(Math.random() * hex.length)];
+    }
+    setColor(hexColor);
+  }
+
+  function handleRgb() {
+    const r = randomColor(256);
+    const g = randomColor(256);
+    const b = randomColor(256);
+
+    setColor(`rgb(${r},${g},${b})`);
+  }
+
+  useEffect(() => {
+    if (typeOfColor === "RGB") handleRgb();
+    else handleHex();
+  }, [typeOfColor]);
 
   return (
     <div
@@ -11,9 +39,15 @@ export default function RandomColor() {
         background: color,
       }}
     >
-      <button>HEX-COLOR</button>
-      <button>RGB-COLOR</button>
-      <button>GENERATE COLOR</button>
+      <button onClick={() => setTypeOfColor("HEX")}>
+        {typeOfColor === "HEX" ? "HEX-Enabled" : "HEX-COLOR"}
+      </button>
+      <button onClick={() => setTypeOfColor("RGB")}>
+        {typeOfColor === "RGB" ? "RGB-Enabled" : "RGB-COLOR"}
+      </button>
+      <button onClick={typeOfColor === "HEX" ? handleHex : handleRgb}>
+        GENERATE COLOR
+      </button>
     </div>
   );
 }
