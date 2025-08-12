5bf38cc01cb708be26e95e6d848c9494dfc20619
Talupula Naveen <stylistar05@gmail.com>
2025-08-12 12:02:54 +0530

    add default background color using useState

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,6 +1,16 @@
+import React, { useState } from "react";
+
 export default function RandomColor() {
+  const [color, setColor] = useState("black");
+
   return (
-    <div>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: color,
+      }}
+    >
       <button>HEX-COLOR</button>
       <button>RGB-COLOR</button>
       <button>GENERATE COLOR</button>
