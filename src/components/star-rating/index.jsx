8b04006d492d5a2a0db285279175dc7b753a2d59
Talupula Naveen <stylistar05@gmail.com>
2025-08-13 12:53:28 +0530

    Add all rating functionalities and also add necessary css

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,41 +1,50 @@
+import { useState } from "react";
+import "./styles.css";
 export default function StarRating({ stars = 5 }) {
+  const [rate, setRate] = useState(0);
+  const [hover, setHover] = useState(0);
+
   const handleClick = (comingIndex) => {
-    console.log(comingIndex);
+    setRate(comingIndex);
   };
   const handleHover = (comingIndex) => {
-    console.log(comingIndex);
+    setHover(comingIndex);
   };
-  const handleLeave = (comingIndex) => {
-    console.log(comingIndex);
-  };
-
+  const handleLeave = () => setHover(0);
   return (
-    <div className="stars">
-      {Array.from({ length: stars }).map((_, index) => (
-        <Icon
-          key={index}
-          onClick={() => handleClick(index)}
-          onMouseMove={() => handleHover(index)}
-          onMouseLeave={() => handleLeave(index)}
-          size={40}
-        />
-      ))}
+    <div className="rateContainer">
+      <h2>Star-Rating</h2>
+      <div className="stars" onMouseLeave={handleLeave}>
+        {Array.from({ length: stars }).map((_, index) => {
+          const starIndex = index + 1;
+          return (
+            <Icon
+              key={starIndex}
+              className={starIndex <= (hover || rate) ? "active" : "inActive"}
+              onClick={() => handleClick(starIndex)}
+              onMouseEnter={() => handleHover(starIndex)}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
 
-function Icon({ onClick, onMouseMove, onMouseLeave, size }) {
+function Icon({ className, onClick, onMouseEnter }) {
   return (
-    <button
+    <div
+      className={className}
       onClick={onClick}
-      onMouseMove={onMouseMove}
-      onMouseLeave={onMouseLeave}
+      onMouseEnter={onMouseEnter}
       style={{
         borderRadius: "50px",
         width: "50px",
         height: "50px",
         marginTop: "50px",
       }}
-    ></button>
+    >
+      ★
+    </div>
   );
 }
