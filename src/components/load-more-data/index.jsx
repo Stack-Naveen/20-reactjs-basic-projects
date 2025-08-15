9e0be23820177b7dbd6ca2e44e73bdb0312c81a7
Talupula Naveen <stylistar05@gmail.com>
2025-08-15 10:20:27 +0530

    fetch products data

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 const LoadMoreData = () => {
+  const [products, setProducts] = useState([]);
+
+  const url = `https://dummyjson.com/products?limit=10&select=title,price,thumbnail`;
+
+  async function fetchUrl(comingUrl) {
+    const response = await fetch(comingUrl);
+    const { products } = await response.json();
+    setProducts(products);
+    console.log(products);
+  }
+  useEffect(() => {
+    fetchUrl(url);
+  }, [url]);
+
   return (
     <div>
-      <h3>Data</h3>
+      <div className="entire-container">
+        {products.map((eachItem) => {
+          const { id, title, price, thumbnail } = eachItem;
+          return (
+            <div>
+              <div className="products-container" key={id}>
+                <div>{title}</div>
+                <img src={thumbnail} alt="product-thumbnail" />
+                <div>{price}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
       <button className="loadBtn">Load More</button>
     </div>
   );
 };
 export default LoadMoreData;
-
