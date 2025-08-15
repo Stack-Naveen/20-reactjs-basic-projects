49764d7dc986faaef871e266e9d09772c850adfc
Talupula Naveen <stylistar05@gmail.com>
2025-08-15 10:14:57 +0530

    Create LoadMoreData component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import ImageSlider from "./components/image-slider";
+/* import ImageSlider from "./components/image-slider"; */
+import LoadMoreData from "./components/load-more-data";
 /* import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color"; */
 /* import StarRating from "./components/star-rating"; */
@@ -9,7 +10,8 @@ function App() {
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
       {/* <StarRating stars="10"/> */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} /> */}
+      <LoadMoreData/>
     </div>
   );
 }
---
+++ src/components/load-more-data/index.jsx
@@ -0,0 +1,10 @@
+const LoadMoreData = () => {
+  return (
+    <div>
+      <h3>Data</h3>
+      <button className="loadBtn">Load More</button>
+    </div>
+  );
+};
+export default LoadMoreData;
+
