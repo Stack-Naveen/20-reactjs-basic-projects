cdd7ae2034a3fdc77623ebece1d30969d968aeea
Talupula Naveen <stylistar05@gmail.com>
2025-08-16 22:47:06 +0530

    Generate QR code

--- src/components/qr-code-generator/index.jsx
+++ src/components/qr-code-generator/index.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
+import { QRCode } from "react-qr-code";
 
 const QrCode = () => {
   const [input, setInput] = useState("");
+  const [qr, setQr] = useState("");
 
+  const handleQr = () => {
+    setQr(input);
+    setInput("");
+  };
   return (
     <div>
       <h2>QR-GENERATOR</h2>
@@ -16,7 +22,15 @@ const QrCode = () => {
           }}
         ></input>
 
-        <button>Generate</button>
+        <button
+          disabled={input && input.trim() !== "" ? false : true}
+          onClick={handleQr}
+        >
+          Generate
+        </button>
+      </div>
+      <div style={{ background: "white", padding: "30px" }}>
+        <QRCode id="qr-code" value={qr} size={300} bgColor="#bac3f6ff" />
       </div>
     </div>
   );
