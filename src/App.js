7c2919e91cf414348d6260ff8356511d4cb7291c
Talupula Naveen <stylistar05@gmail.com>
2025-08-16 22:41:41 +0530

    create QR component and add sample input with a button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import TreeView from "./components/tree-view";
-import menus from "./components/tree-view/data";
+/* import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data"; */
 /* import ImageSlider from "./components/image-slider"; */
 /* import LoadMoreData from "./components/load-more-data"; */
 /* import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color"; */
 /* import StarRating from "./components/star-rating"; */
+import QrCode from "./components/qr-code-generator";
 function App() {
   return (
     <div className="App">
@@ -14,7 +15,8 @@ function App() {
       {/* <StarRating stars="10"/> */}
       {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} /> */}
       {/* <LoadMoreData/> */}
-      <TreeView menus={menus}/>
+      {/* <TreeView menus={menus}/> */}
+      <QrCode/>
     </div>
   );
 }
---
+++ src/components/qr-code-generator/index.jsx
@@ -0,0 +1,17 @@
+const QrCode = () => {
+  return (
+    <div>
+      <h2>QR-GENERATOR</h2>
+      <div>
+        <input
+          type="text"
+          value="check"
+          placeholder="type something . ."
+        ></input>
+
+        <button>Generate</button>
+      </div>
+    </div>
+  );
+};
+export default QrCode;
