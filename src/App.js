6ae7907a6c41f1a01347693e6fad54fa517aa0db
Talupula Naveen <stylistar05@gmail.com>
2025-08-16 09:59:28 +0530

    Take dummy data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
+import TreeView from "./components/tree-view";
 /* import ImageSlider from "./components/image-slider"; */
-import LoadMoreData from "./components/load-more-data";
+/* import LoadMoreData from "./components/load-more-data"; */
 /* import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color"; */
 /* import StarRating from "./components/star-rating"; */
@@ -11,7 +12,7 @@ function App() {
       {/* <RandomColor /> */}
       {/* <StarRating stars="10"/> */}
       {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} /> */}
-      <LoadMoreData/>
+      {/* <LoadMoreData/> */}
     </div>
   );
 }
