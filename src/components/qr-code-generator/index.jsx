30e2abf815f442c2a84d5c84cb4cba917c132f74
Talupula Naveen <stylistar05@gmail.com>
2025-08-16 22:44:40 +0530

    update input

--- src/components/qr-code-generator/index.jsx
+++ src/components/qr-code-generator/index.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 const QrCode = () => {
+  const [input, setInput] = useState("");
+
   return (
     <div>
       <h2>QR-GENERATOR</h2>
       <div>
         <input
           type="text"
-          value="check"
+          value={input}
           placeholder="type something . ."
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
         ></input>
 
         <button>Generate</button>
