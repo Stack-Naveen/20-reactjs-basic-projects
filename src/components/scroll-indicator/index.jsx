c9eab43277d22e31f18db338b28134dd0dd41348
Talupula Naveen <stylistar05@gmail.com>
2025-08-19 12:32:18 +0530

    Add loading and error handling

--- src/components/scroll-indicator/index.jsx
+++ src/components/scroll-indicator/index.jsx
@@ -2,28 +2,42 @@ import { useState, useEffect } from "react";
 
 const ScrollIndicator = ({ url }) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
   async function fetchUrl(comingUrl) {
-    const response = await fetch(comingUrl);
-    const { products } = await response.json();
-    setData(products);
-    console.log(products);
+    setLoading(true);
+    try {
+      const response = await fetch(comingUrl);
+      const { products } = await response.json();
+      setData(products);
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      setError(e.message);
+    }
   }
 
   useEffect(() => {
     fetchUrl(url);
   }, [url]);
 
+  if (loading) return <h3>Loading . . .</h3>;
+  if (error) return <h3>{error}</h3>;
+
   return (
     <div>
       <h2>Scroll-Indicator</h2>
-      {data.map(({ id, title, category }) => {
-        return (
-          <div key={id}>
-            <h3>{title}</h3>
-            <p>Category - {category}</p>
-          </div>
-        );
-      })}
+      {data && data.length
+        ? data.map(({ id, title, category }) => {
+            return (
+              <div key={id}>
+                <h3>{title}</h3>
+                <p>Category - {category}</p>
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 };
